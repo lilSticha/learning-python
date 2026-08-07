f4f18236426860104b7f8e85012735c9d2386b4a
ivans <ivan@gmai.com>
2026-08-07 17:09:59 +0300

    push calculator

---
+++ calculator.py
@@ -0,0 +1,14 @@
+a = float(input('First number:'))
+b = float(input('Second number:'))
+
+sign = input('Enter the operation (+, -, *, /):')
+if sign == '+':
+    print(a+b)
+elif sign == '-':
+    print(a-b)
+elif sign == '*':
+    print(a*b)
+elif sign == '/':
+    print(a/b)
+else : 
+    print('Invalid operation')
